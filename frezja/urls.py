2d54a9a6d1ad4a95b70636e7bfd512aefadb80cd
oathbringerUUU <marcin.swidzinski7@gmail.com>
2024-06-26 12:36:00 +0200

    dodano routing do home

--- frezja/urls.py
+++ frezja/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("home.urls")),
     path("posts/", include("posts.urls")),
     path("photos/", include("photos.urls")),
 ]
