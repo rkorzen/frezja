3fdaf88e7705cc2ab51ac3b0200edc36a2cd0970
Bula23 <markowski.ad@gmail.com>
2024-06-26 03:27:29 -0700

    flake8 ready

--- comments/models.py
+++ comments/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
-
-
-
 class Comment(models.Model):
     class Meta:
         abstract = True
@@ -11,6 +9,7 @@ class Comment(models.Model):
     email = models.EmailField()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class PostComment(Comment):
     post = models.ForeignKey("posts.Post", on_delete=models.CASCADE, related_name="comments")
