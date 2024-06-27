34334c5034ed915bad3d31c66f9e41fec6798883
rkorzen <korzeniewski@gmail.com>
2024-06-27 09:39:01 +0200

    paginacja

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import HttpResponse, render
 from photos.models import Photo
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def list(request):
+
+    photos = Photo.objects.all()
+    paginator = Paginator(photos, 20)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         "photos/list.html",
-        dict(photos=Photo.objects.all())
+        dict(photos=page_obj.object_list, page=page_obj)
     )
 
 
