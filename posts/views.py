5d361f9916d4097dfa5c3e5a24125106917c3ab6
Bula23 <markowski.ad@gmail.com>
2024-06-27 00:52:49 -0700

    pagincja widoku postow

--- posts/views.py
+++ posts/views.py
@@ -2,27 +2,21 @@
 from django.shortcuts import render
 
 from posts.models import Post
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def list(request):
     posts = Post.objects.all()
-    dct = dict()
-    lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    index = 1
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    dct['key'] = lst[index]
 
     return render(
-        request,
-        "posts/list.html",
-        {
-            "posts": posts,
-            "slownik": dct,
-            "lista": lst,
-            "napis": "tekst jakiś tma",
-        }
+      request,
+     "posts/list.html",
+      dict(posts=page_obj.object_list, page=page_obj)
     )
 
 
